9150d2f18608c55d7214e01d64d1b7aa6d9a2978
Ethan Emiliano <jimimorison03@gmail.com>
2022-05-27 01:21:37 -0500

    HOAL

---
+++ pr19_05/ajax.php
@@ -0,0 +1,8 @@
+<?php
+
+    $usuario = $_POST["ususario"];
+    $contrasena = $_POST["contrasena"];
+    $correo = $_POST["correo"]
+
+
+?>
---
+++ pr19_05_2/ajax.php
@@ -0,0 +1,9 @@
+<?php
+
+    $usuario = $_POST["ususario"];
+    $contrasena = $_POST["contrasena"];
+    $correo = $_POST["correo"]
+
+    echo "<ul><li></li><li></li>"
+
+?>
